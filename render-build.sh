424c30e3dfc4749ead48e9d58c2172ef4bf61ea9
Jaros-777 <filip.jarocki@wp.pl>
2025-03-13 18:56:33 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,38 @@
 #!/usr/bin/env bash
-# exit on errorset -o errexit
+# exit on errors
+set -o errexit
 
+# Instalacja zależności projektu
 npm install
-# npm run build # uncomment if required
+# npm run build # Uncomment if potrzebne
 
-# Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "...Copying Puppeteer Cache from Build Cache" 
+# Instalacja Google Chrome i brakujących zależności
+sudo apt-get update
+sudo apt-get install -y \
+  google-chrome-stable \
+  libasound2 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  libxss1 \
+  libxtst6 \
+  fonts-liberation \
+  libnss3 \
+  xdg-utils
+
+# Debugowanie ścieżki Chrome
+echo "Ścieżka Google Chrome:"
+which google-chrome-stable
+
+# Obsługa Puppeteer Cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+  echo "...Copying Puppeteer Cache from Build Cache"
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "...Storing Puppeteer Cache in Build Cache" 
+else
+  echo "...Storing Puppeteer Cache in Build Cache"
   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
 fi
